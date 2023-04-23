9e6b8c9fc2af6d7058daf6f9eadc4f4e33070c3e
Fazekas Adrienn <adreeja17@gmail.com>
2023-04-23 16:58:55 +0200

    fetch

---
+++ index.js
@@ -0,0 +1,8 @@
+
+document.getElementById("newdeck-btn").addEventListener("click", handleClick)
+
+function handleClick() {
+    fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
+        .then(res => res.json())
+        .then(data => console.log(data))
+}
