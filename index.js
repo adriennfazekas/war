9d75eaf47870b4e9b649a6991f7cb325cf154e32
Fazekas Adrienn <adreeja17@gmail.com>
2023-04-24 17:40:51 +0200

    draw 2 card from the deck

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 
+let deckId
+
 document.getElementById("newdeck-btn").addEventListener("click", handleClick)
+document.getElementById("draw-btn").addEventListener("click", drawCard)
 
 function handleClick() {
     fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
         .then(res => res.json())
-        .then(data => console.log(data))
+        .then(data => {
+            deckId = data.deck_id
+            console.log(data)
+        })
+}
+
+function drawCard() {
+    fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data.cards[0], data.cards[1])
+        })
 }
