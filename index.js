2c6ac90e9397bde9163b8bebe6adcbb6a36b04af
Fazekas Adrienn <adreeja17@gmail.com>
2023-04-29 00:14:29 +0200

    count the score and display who won the game

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 
 let deckId
+let computerScore = 0
+let humanScore = 0
 const cardsContainer = document.getElementById("card-field")
+const gameStatus = document.getElementById("game-status")
+const remainingCard = document.getElementById("remaining")
+const drawBtn = document.getElementById("draw-btn")
+const computer = document.getElementById("computer")
+const human = document.getElementById("human")
 
 document.getElementById("newdeck-btn").addEventListener("click", handleClick)
-document.getElementById("draw-btn").addEventListener("click", drawCard)
+drawBtn.addEventListener("click", drawCard)
 
 function handleClick() {
     fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
         .then(res => res.json())
         .then(data => {
+            remainingCard.innerText = `Remaining cards: ${data.remaining}`
             deckId = data.deck_id
         })
 }
@@ -22,7 +30,21 @@ function drawCard() {
             cardsContainer.children[1].innerHTML = `
             <img src=${data.cards[1].image} class="drawn-card" />`
 
-            document.getElementById("game-status").innerText = handleCard(data.cards[0], data.cards[1])
+            gameStatus.innerText = handleCard(data.cards[0], data.cards[1])
+            remainingCard.innerText = `Remaining cards: ${data.remaining}`
+
+            if (data.remaining === 0) {
+                drawBtn.disabled = true
+                remainingCard.innerText = ""
+                
+                if (humanScore > computerScore) {
+                    gameStatus.innerText = "YOU won the game!"
+                } else if (humanScore < computerScore) {
+                    gameStatus.innerText = "COMPUTER won the game!"
+                } else {
+                    gameStatus.innerText = "It is a tie!"
+                }
+            }
         })
 }
 
@@ -32,10 +54,14 @@ function handleCard(compCard, meCard) {
 
     const compCardIndex = valueOptions.indexOf(compCard.value)    
     const meCardIndex = valueOptions.indexOf(meCard.value)
-    
+
     if(compCardIndex > meCardIndex) {
+        computerScore++
+        computer.innerText = `Computer: ${computerScore}`
         return "Computer is the winner"
     } else if(compCardIndex < meCardIndex) {
+        humanScore++
+        human.innerText = `Me: ${humanScore}`
         return "You are the winner"
     } else return "War!"
 }
